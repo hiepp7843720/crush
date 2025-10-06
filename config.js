6c49057e6dcee74deeac00eeb59589c961eef9da
hiepp7843720 <duyhiep063@gmail.com>
2025-10-06 19:09:56 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "PhuongLe xinhiu",
+    introTitle: 'PhuongLe xinhiu😘',
+    introDesc: `
+    Phượng dịu dàng làm tim anh xao xuyến
+
+Anh nhẹ nhàng tiến đến trái tim em`,
+    btnIntro: '^^HiHi^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'chắc chắn vậy ròi hẹ hẹ <33',
+    btnNo: 'Không nha :3',
+    question: 'Ựa đói quá gửi anh một chút ngọt ngào điii <3',
+    btnReply: 'Gửi cho tiểu bảo bối <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'ありがとう、ベイビー 🥰. Yêu béiu nhiều nhiều 😘😘',
+    messDesc: 'bé có iu a hongg có thì lên phím 1 nhaaa',
+    btnAccept: '1 nè <3',
+     messLink: ''
+}
